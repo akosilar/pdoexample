d03d30520b56067e52a2e3aff986d80b2c6f93d4
akosilar <55524614+akosilar@users.noreply.github.com>
2023-09-26 23:47:44 -0400

    named params and positional params

--- index.php
+++ index.php
@@ -9,11 +9,47 @@
 
     //create a pdo instance
     $pdo = new PDO($dsn,$user,$password);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
 
     // pdo query
 
-    $stmt = $pdo->query('SELECT * FROM posts');
+    // $stmt = $pdo->query('SELECT * FROM posts');
 
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        echo $row['title'] . '<br>';
+    // // while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    // //     echo $row['title'] . '<br>';
+    // // }
+
+    // while($row = $stmt->fetch()){
+    //     echo $row->title . ' '. $row->body . '<br>';
+        
+    // }
+
+
+    //prepared statements (prepare & execute)
+
+    // unsafe
+    // $sql = "SELECT * FROM posts WHERE author = '$author'";
+
+    // fetch multiple posts
+
+    //user input
+    $author = "mars";
+
+    // positional params
+    // $sql = 'SELECT * FROM posts where author = ?';
+    // $stmt = $pdo->prepare($sql);
+    // $stmt->execute([$author]);
+    // $posts = $stmt->fetchAll();
+    
+    // named params
+    $sql = 'SELECT * FROM posts where author = :author';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['author' => $author]);
+    $posts = $stmt->fetchAll();
+
+    // var_dump($posts);
+    
+    foreach ($posts as $post ) {
+        echo $post->title . '<br>';
     }
+    
